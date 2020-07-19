5ab086362bd073bf4f23f19709d0698f5324a4f3
lionzxy <nikita@kulikof.ru>
2020-07-19 17:15:15 +0300

    Some small fix

--- app/src/main/java/ru/lionzxy/alarmonnotification/MainActivity.kt
+++ app/src/main/java/ru/lionzxy/alarmonnotification/MainActivity.kt
@@ -1,13 +1,17 @@
 package ru.lionzxy.alarmonnotification
 
+import android.Manifest
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
-import ru.lionzxy.alarmonnotification.alarm.AlarmKlaxon
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +25,16 @@ class MainActivity : AppCompatActivity() {
                 startActivity(Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS))
             }
         }
-        startActivity(Intent(this, AlarmActivity::class.java))
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, arrayOf(WRITE_EXTERNAL_STORAGE), 0);
+        } else {
+            startActivity(Intent(this, AlarmActivity::class.java))
+        }
+
     }
 }
--- app/src/main/java/ru/lionzxy/alarmonnotification/NotificationService.kt
+++ app/src/main/java/ru/lionzxy/alarmonnotification/NotificationService.kt
@@ -1,32 +1,50 @@
 package ru.lionzxy.alarmonnotification
 
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.os.Environment
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
 import android.widget.Toast
+import java.io.File
+import java.util.concurrent.TimeUnit
 
+var lastActive = 0L
 
 public class NotificationService : NotificationListenerService() {
+
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
         val packageName = sbn?.packageName ?: return
         val extras = sbn.notification.extras ?: return
         var titleData = ""
         var textData = ""
 
-        if (extras.getString("android.title") != null) {
-            titleData = extras.getString("android.title") ?: ""
+        if (extras.getCharSequence("android.title") != null) {
+            titleData = extras.getCharSequence("android.title")?.toString() ?: ""
         }
         if (extras.getCharSequence("android.text") != null) {
-            textData = extras.getCharSequence("android.text").toString();
+            textData = extras.getCharSequence("android.text")?.toString() ?: ""
         }
 
         Log.d("Package", packageName);
         Log.d("Title", titleData);
         Log.d("Text", textData);
-        if (packageName.contains("ua.gov.diia.quarantine", true)) {
+        val file =
+            File(Environment.getExternalStorageDirectory(), "NotificationAlarm/notification.log")
+        if (!file.exists()) {
+            file.createNewFile()
+        }
+        val isUA = packageName.contains("ua.gov.diia.quarantine", true)
+        val valid = (System.currentTimeMillis() - lastActive) > TimeUnit.MINUTES.convert(1, TimeUnit.MILLISECONDS)
+        file.appendText("New notification from $packageName (is ua: $isUA/$valid) with title \"$titleData\" with text \"$textData\"\n")
+        file.appendText("==================================================================\n\t")
+        if (isUA && valid) {
+            lastActive = System.currentTimeMillis()
             Toast.makeText(applicationContext, titleData, Toast.LENGTH_LONG).show()
-            startActivity(Intent(applicationContext, AlarmActivity::class.java))
+            val intent = Intent(baseContext, AlarmActivity::class.java)
+            intent.flags = FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
         }
     }
 }
